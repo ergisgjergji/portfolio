d2bf8433ba47755d745da44030051c53ddb4d0d7
Ergis Gjergji <ergis.gjergji@italwebconsulting.al>
2022-07-13 18:26:23 +0200

    commented projects route

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -23,9 +23,9 @@ const AppRouter = (props) => {
                 <Resume />
             } />
 
-            <Route path={MyRoutes.Projects} element={
+            {/* <Route path={MyRoutes.Projects} element={
                 <Projects />
-            } />
+            } /> */}
 
       </Routes>
     )
